7a3b9137e409a8b0c044243aaa23c6fe2215d396
zina <keiraayari@gmail.com>
2020-09-23 16:44:55 +0200

    ajout_Slider

---
+++ elsy/src/Core/Slider.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+//import Slider, { Range } from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import RCSlider from 'rc-slider';
+
+
+
+class Slider extends Component {
+    render() {
+        return (
+            <div className="conteneur">
+            <RCSlider max={this.props.max}
+                min={this.props.min}
+                onChange={this.props.onChange}
+                value={this.props.value} />
+                </div>
+        );
+    }
+}
+
+
+
+
+
+export default Slider;
+
